d4831a45e80ab1918a11fae1fe93e6ef70f82a00
Stargazer777777 <91822258+Stargazer777777@users.noreply.github.com>
2023-09-24 14:41:39 +0000

    Deploying to gh-pages from @ Stargazer777777/blog@4b9c58f53981b96bdf6f8e99a8d9a4b7538f2446 🚀

---
+++ assets/8.枚举类型.html-eae0948e.js
@@ -0,0 +1,15 @@
+import{_ as n}from"./plugin-vue_export-helper-c27b6911.js";import{o as s,c as e,f as a}from"./app-7256f6ed.js";const t={},i=a(`<h4 id="语法" tabindex="-1"><a class="header-anchor" href="#语法" aria-hidden="true">#</a> 语法</h4><div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code><span class="token keyword">enum</span> Color <span class="token punctuation">{</span>
+    red<span class="token punctuation">,</span>
+    blue<span class="token operator">=</span><span class="token number">2</span><span class="token punctuation">,</span>
+    green
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h4 id="例子" tabindex="-1"><a class="header-anchor" href="#例子" aria-hidden="true">#</a> 例子</h4><div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code><span class="token keyword">enum</span> Color <span class="token punctuation">{</span>
+    red<span class="token punctuation">,</span>
+    blue<span class="token operator">=</span><span class="token number">2</span><span class="token punctuation">,</span>
+    green
+<span class="token punctuation">}</span>
+
+<span class="token builtin">console</span><span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>Color<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token comment">// expect output</span>
+<span class="token comment">// { &#39;0&#39;: &#39;red&#39;, &#39;2&#39;: &#39;blue&#39;, &#39;3&#39;: &#39;green&#39;, red: 0, blue: 2, green: 3 }</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),c=[i];function l(o,p){return s(),e("div",null,c)}const u=n(t,[["render",l],["__file","8.枚举类型.html.vue"]]);export{u as default};
